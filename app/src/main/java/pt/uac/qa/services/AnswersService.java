22f84ac6b9bfa787e59ef5e1c3b267ed22fcef54
Olamundo21 <20170014@aluno.uac.pt>
2020-01-18 17:26:35 -0100

    AnswersService feito expecto o voto up/down e o markAnswerCorrect.

--- app/src/main/java/pt/uac/qa/services/AnswersService.java
+++ app/src/main/java/pt/uac/qa/services/AnswersService.java
@@ -24,19 +24,23 @@ import static android.content.ContentValues.TAG;
 public class AnswersService extends IntentService {
     // TODO: Rename actions, choose action names that describe tasks that this
     // IntentService can perform, e.g. ACTION_FETCH_NEW_ITEMS
-    private static final String ACTION_FETCH_MY_ANSWERS = "pt.uac.qa.services.action.FETCH_ANSWERS";
-    private static final String ACTION_ADD_ANSWERS = "pt.uac.qa.services.action.ADD_ANSWERS";
+    private static final String ACTION_FETCH_ANSWER = "pt.uac.qa.services.action.FETCH_ANSWER";
+    private static final String ACTION_FETCH_MY_ANSWERS = "pt.uac.qa.services.action.FETCH_MY_ANSWERS";
+    private static final String ACTION_ADD_ANSWER = "pt.uac.qa.services.action.ADD_ANSWER";
     private static final String ACTION_DELETE_ANSWERS = "pt.uac.qa.services.action.DELETE_ANSWERS";
-    private static final String ACTION_UPDATE_ANSWERS = "pt.uac.qa.services.action.UPDATE_ANSWERS";
+    private static final String ACTION_UPDATE_ANSWER = "pt.uac.qa.services.action.UPDATE_ANSWER";
 
     // TODO: Rename parameters
     private static final String EXTRA_PARAM_BODY = "pt.uac.qa.services.extra.PARAM_BODY";
-    private static final String EXTRA_PARAM_ANSWERS_IDS = "pt.uac.qa.services.extra.PARAM_ANSWERS_IDS";
     private static final String EXTRA_PARAM_QUESTION_ID = "pt.uac.qa.services.extra.PARAM_QUESTION_ID";
+    private static final String EXTRA_PARAM_ANSWER_ID = "pt.uac.qa.services.PARAM_ANSWER_ID";
+    private static final String EXTRA_PARAM_ANSWERS_IDS = "pt.uac.qa.services.extra.PARAM_ANSWERS_IDS";
 
     public static final String INTENT_FILTER = "pt.uac.qa.services.QUESTION_SERVICE";
     public static final String RESULT_ERROR = "pt.uac.qa.services.RESULT_ERROR";
-    public static final String RESULT_ANSWERS = "pt.uac.qa.services.RESULT_QUESTIONS";
+    public static final String RESULT_ANSWERS = "pt.uac.qa.services.RESULT_ANSWERS";
+    public static final String RESULT_ANSWER = "pt.uac.qa.services.RESULT_ANSWER";
+
 
     public AnswersService() {
         super("AnswersService");
@@ -48,27 +52,43 @@ public class AnswersService extends IntentService {
      *
      * @see IntentService
      */
-    // VAI BUSCAR AS RESPOSTAS
 
+    //  VAI BUSCAR UMA RESPOSTA QUANDO FOR PARA ABRIR PARA VER O TEXTO.
+    public static void fetchAnswer(Context context, String answerId) {
+        Intent intent = new Intent(context, AnswersService.class);
+        intent.setAction(ACTION_FETCH_ANSWER);
+        intent.putExtra(EXTRA_PARAM_ANSWER_ID, answerId);
+        context.startService(intent);
+    }
+
+    // VAI BUSCAR AS RESPOSTAS
     public static void fetchMyAnswers(Context context) {
-        Intent intent = new Intent(context, QuestionService.class);
+        Intent intent = new Intent(context, AnswersService.class);
         intent.setAction(ACTION_FETCH_MY_ANSWERS);
         context.startService(intent);
     }
 
-
-    // VAI ADICIONAR RESPOSTAS
-    public static void addAnswers(Context context, String body) {
-        Intent intent = new Intent(context, QuestionService.class);
-        intent.setAction(ACTION_ADD_ANSWERS);
+    // VAI ADICIONAR UMA RESPOSTA
+    public static void addAnswer(Context context, String body) {
+        Intent intent = new Intent(context, AnswersService.class);
+        intent.setAction(ACTION_ADD_ANSWER);
         intent.putExtra(EXTRA_PARAM_BODY, body);
         context.startService(intent);
     }
 
     // VAI ELIMINAR RESPOSTAS
     public static void deleteAnswers(Context context, String answerId){
-        Intent intent = new Intent(context, QuestionService.class);
+        Intent intent = new Intent(context, AnswersService.class);
         intent.setAction(ACTION_DELETE_ANSWERS);
+        context.startService(intent);
+    }
+
+    public static void updateAnswer(Context context, String answerId, String body){
+        Intent intent = new Intent(context, AnswersService.class);
+        intent.setAction(ACTION_UPDATE_ANSWER);
+        intent.putExtra(EXTRA_PARAM_ANSWER_ID, answerId);
+        intent.putExtra(EXTRA_PARAM_BODY, body);
+        context.startService(intent);
     }
 
     // HANDLER *****
@@ -76,14 +96,24 @@ public class AnswersService extends IntentService {
     protected void onHandleIntent(Intent intent) {
         if (intent != null) {
             final String action = intent.getAction();
+
             if (ACTION_FETCH_MY_ANSWERS.equals(action)) {
                 getMyAnswers();
-            } else if ((ACTION_UPDATE_ANSWERS.equals(action))) {
-                updateAnswer();
-            } else if (ACTION_ADD_ANSWERS.equals(action)) {
+
+            }else if ((ACTION_FETCH_ANSWER.equals(action))) {
+                String answerId = intent.getStringExtra(EXTRA_PARAM_ANSWER_ID);
+                getAnswer(answerId);
+
+            } else if ((ACTION_UPDATE_ANSWER.equals(action))) {
+                String answerId = intent.getStringExtra(EXTRA_PARAM_ANSWER_ID);
+                String body = intent.getStringExtra(EXTRA_PARAM_BODY);
+                updateAnswer(answerId, body);
+
+            } else if (ACTION_ADD_ANSWER.equals(action)) {
                 String questionId = intent.getStringExtra(EXTRA_PARAM_QUESTION_ID);
                 String body = intent.getStringExtra(EXTRA_PARAM_BODY);
                 addAnswer(questionId,body);
+
             } else if (ACTION_DELETE_ANSWERS.equals((action))){
                 String[] answersIds = intent.getStringArrayExtra(EXTRA_PARAM_ANSWERS_IDS);
                 deleteAnswers(answersIds);
@@ -91,8 +121,7 @@ public class AnswersService extends IntentService {
         }
     }
 
-    // METODOS PRIVADOS
-
+    // RECEBER AS MINHAS RESPOSTAS
     private void getMyAnswers() {
         try {
             final AnswerClient client = new AnswerClient(this);
@@ -103,6 +132,21 @@ public class AnswersService extends IntentService {
         }
     }
 
+    //  RECEBER RESPOSTA
+    private void getAnswer(String answerId) {
+            try {
+                AnswerClient client= new AnswerClient(this);
+                Answer answer = client.getAnswer(answerId);
+                Intent intent = new Intent(INTENT_FILTER);
+                intent.putExtra(RESULT_ANSWER, answer);
+                sendBroadcast(intent);
+            } catch (ClientException e) {
+                Log.e(TAG, null, e);
+                sendErrorBroadcast(e);
+            }
+    }
+
+    // ADICIONAR RESPOSTA
     private void addAnswer(String questionId,String body) {
         try {
             final AnswerClient client = new AnswerClient(this);
@@ -114,6 +158,7 @@ public class AnswersService extends IntentService {
         }
     }
 
+    // ELIMINAR RESPOSTAS
     private void deleteAnswers(String[] answersIds) {
             try {
                 AnswerClient client = new AnswerClient(this);
@@ -127,8 +172,16 @@ public class AnswersService extends IntentService {
             }
         }
 
-    private void updateAnswer() {
-
+    // ACTUALIZAR RESPOSTA
+    private void updateAnswer(String answerId, String body) {
+        try {
+            AnswerClient client = new AnswerClient(this);
+            client.updateAnswer(answerId, body);
+            sendBroadcast(new Intent(INTENT_FILTER));
+        } catch (ClientException e) {
+            Log.e(TAG, null, e);
+            sendErrorBroadcast(e);
+        }
     }
 
     // ERROR BROADCASTS **
